bd76dbef00e59da865f0ba82c89aa267d9f6784f
Vicwin13 <vnwimo13@gmail.com>
2022-06-22 14:04:23 +0100

    yhh

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let fullname = document.getElementById("name");
+let myName = "Nwimo Victor";
 let height = 170;
 let country = "Nigeria";
 
 
-fullname.innerHTML = `My name is Nwimo Victor and I'm ${height}cm tall and I come from ${country}`;
+fullname.innerHTML = `My name is ${myName} and I'm ${height}cm tall and I come from ${country}`;
