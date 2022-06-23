e16f4601f94d9db59d22e0e7a191892fba1f5637
Vicwin13 <vnwimo13@gmail.com>
2022-06-23 15:19:45 +0100

    again

--- script.js
+++ script.js
@@ -6,4 +6,4 @@ let country = "Nigeria";
 
 fullname.innerHTML = `My name is ${myName}, I'm ${height}cm tall and I come from ${country}`;
 
-document.write(`Github link:  https://github.com/Vicwin13/DisplayJs`)
+document.write(`Github link-:  https://github.com/Vicwin13/DisplayJs`)
